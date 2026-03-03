aae5b559969094ab4448ad51f1dbd40262b6388a
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-03 08:40:12 +0800

    Add Result2D component to fetch and display results

--- src/components/Result2D.js
+++ src/components/Result2D.js
@@ -1,1 +1,23 @@
 
+import React, { useEffect, useState } from "react";
+
+export default function Result2D() {
+
+  const [result, setResult] = useState("--");
+
+  useEffect(() => {
+    fetch("https://api.mocki.io/v2/549a5d8b/2D")
+      .then(res => res.json())
+      .then(data => {
+        setResult(data.result || "00");
+      })
+      .catch(() => setResult("Error"));
+  }, []);
+
+  return (
+    <div className="card">
+      <h2>Myanmar 2D Result</h2>
+      <h1>{result}</h1>
+    </div>
+  );
+}
